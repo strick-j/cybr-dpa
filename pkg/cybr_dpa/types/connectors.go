c65f773ae842bdba207662770839dc36925ac6b7
Joe Strickland <strick-j@users.noreply.github.com>
2023-03-28 13:26:03 -0400

    updated types.connectors

--- pkg/cybr_dpa/types/connectors.go
+++ pkg/cybr_dpa/types/connectors.go
@@ -1,8 +1,11 @@
 package types
 
 type ConnectorScript struct {
-	ScriptUrl   string `json:"script_url"`
-	BashCommand string `json:"bash_cmd"`
+	ScriptUrl   string `json:"script_url,omitempty"`
+	BashCommand string `json:"bash_cmd,omitempty"`
+	Code        string `json:"code,omitempty"`
+	Message     string `json:"message,omitempty"`
+	Description string `json:"description,omitempty"`
 }
 
 type ConnectorRequest struct {

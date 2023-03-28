8e0c20a740bfff30406fcef8a7c148f1fac237bc
Joe Strickland <strick-j@users.noreply.github.com>
2023-03-28 12:17:05 -0400

    updated platform token url

--- pkg/cybr_dpa/auth.go
+++ pkg/cybr_dpa/auth.go
@@ -23,7 +23,7 @@ func OauthPlatformToken(clientID, clientSecret, clientURL string) (*oauth2.Token
 	var credentialConfig = clientcredentials.Config{
 		ClientID:     clientID,
 		ClientSecret: clientSecret,
-		TokenURL:     "https://" + clientURL + "/oauth2/token/platform",
+		TokenURL:     "https://" + clientURL + "/oauth2/token/platformtoken",
 		AuthStyle:    0,
 		Scopes:       []string{""},
 	}

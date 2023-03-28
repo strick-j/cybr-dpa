53450f7654edd3b5dd1664ddc0426aad016fba7f
Joe Strickland <strick-j@users.noreply.github.com>
2023-03-28 13:52:20 -0400

    Updated settings struct

--- pkg/cybr_dpa/types/settings.go
+++ pkg/cybr_dpa/types/settings.go
@@ -1,14 +1,12 @@
 package types
 
 type Settings struct {
-	FeatureName string      `json:"feature_name,omitempty"`
-	FeatureConf FeatureConf `json:"feature_conf,omitempty"`
-	Code        string      `json:"code,omitempty"`
-	Message     string      `json:"message,omitempty"`
-	Description string      `json:"description,omitempty"`
-}
-
-type FeatureConf struct {
-	IsMfaCacheEnabled    bool `json:"isMfaCachingEnabled,omitempty"`
-	KeyExpirationTimeSec int  `json:"keyExpirationTimeSec,omitempty"`
+	FeatureName string `json:"feature_name,omitempty"`
+	FeatureConf struct {
+		IsMfaCachingEnabled  bool `json:"isMfaCachingEnabled,omitempty"`
+		KeyExpirationTimeSec int  `json:"keyExpirationTimeSec,omitempty"`
+	} `json:"feature_conf,omitempty"`
+	Code        string `json:"code,omitempty"`
+	Message     string `json:"message,omitempty"`
+	Description string `json:"description,omitempty"`
 }

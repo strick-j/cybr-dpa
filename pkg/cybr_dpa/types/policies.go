0ab9f4195e544154cd20adb703b717c9463ba492
Joe Strickland <strick-j@users.noreply.github.com>
2023-03-28 16:20:12 -0400

    Updated Policies type

--- pkg/cybr_dpa/types/policies.go
+++ pkg/cybr_dpa/types/policies.go
@@ -1,11 +1,14 @@
 package types
 
-type Policies []struct {
-	PolicyID    string   `json:"policyId"`
-	Status      string   `json:"status"`
-	PolicyName  string   `json:"policyName"`
-	Description string   `json:"description"`
-	UpdatedOn   string   `json:"updatedOn"`
-	RuleNames   []string `json:"ruleNames"`
-	Platforms   []string `json:"platforms"`
+type Policies struct {
+	Items []struct {
+		PolicyID    string   `json:"policyId"`
+		Status      string   `json:"status"`
+		PolicyName  string   `json:"policyName"`
+		Description string   `json:"description"`
+		UpdatedOn   string   `json:"updatedOn"`
+		RuleNames   []string `json:"ruleNames"`
+		Platforms   []string `json:"platforms"`
+	} `json:"items"`
+	TotalCount int `json:"totalCount"`
 }

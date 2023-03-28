8535be0df3cb7fa9efcf419a6863393425ffbee1
Joe Strickland <strick-j@users.noreply.github.com>
2023-03-28 14:59:17 -0400

    Added Policies

--- pkg/cybr_dpa/policies.go
+++ pkg/cybr_dpa/policies.go
@@ -11,14 +11,27 @@ var (
 	Policies types.Policies
 )
 
-// GetPolcies: Returns all authorization policies
+// GetPolicies: Returns all authorization policies
 //
 // Example Usage:
 //
 //	getPolicies, err := s.GetPolicies(context.Background)
 func (s *Service) GetPolicies(ctx context.Context) (*types.Policies, error) {
-	if err := s.client.Get(ctx, fmt.Sprintf("/%s", "access-polcies"), &Policies); err != nil {
-		return nil, fmt.Errorf("failed to get public key: %w", err)
+	if err := s.client.Get(ctx, fmt.Sprintf("/%s", "access-policies"), &Policies); err != nil {
+		return nil, fmt.Errorf("failed to get policies: %w", err)
+	}
+
+	return &Policies, nil
+}
+
+// GetPolicyById: Retrieves authorization policy for the given ID
+//
+// Example Usage:
+//
+//	getPolicies, err := s.GetPolicyById(context.Background, "{policy_id}")
+func (s *Service) GetPolicyById(ctx context.Context, policyId string) (*types.Policies, error) {
+	if err := s.client.Get(ctx, fmt.Sprintf("/%s/%s", "access-policies", policyId), &Policies); err != nil {
+		return nil, fmt.Errorf("failed to get policy: %w", err)
 	}
 
 	return &Policies, nil

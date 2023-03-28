083af45e2c7db6b6150d399196ddbc1ead3c3dc2
Joe Strickland <strick-j@users.noreply.github.com>
2023-03-28 15:28:06 -0400

    Added additional settings functions

--- pkg/cybr_dpa/types/settings.go
+++ pkg/cybr_dpa/types/settings.go
@@ -1,12 +1,14 @@
 package types
 
 type Settings struct {
-	FeatureName string `json:"feature_name,omitempty"`
-	FeatureConf struct {
-		IsMfaCachingEnabled  bool `json:"isMfaCachingEnabled,omitempty"`
-		KeyExpirationTimeSec int  `json:"keyExpirationTimeSec,omitempty"`
-	} `json:"feature_conf,omitempty"`
-	Code        string `json:"code,omitempty"`
-	Message     string `json:"message,omitempty"`
-	Description string `json:"description,omitempty"`
+	FeatureName string      `json:"feature_name,omitempty"`
+	FeatureConf FeatureConf `json:"feature_conf,omitempty"`
+	Code        string      `json:"code,omitempty"`
+	Message     string      `json:"message,omitempty"`
+	Description string      `json:"description,omitempty"`
+}
+
+type FeatureConf struct {
+	IsMfaCachingEnabled  bool `json:"isMfaCachingEnabled,omitempty"`
+	KeyExpirationTimeSec int  `json:"keyExpirationTimeSec,omitempty"`
 }

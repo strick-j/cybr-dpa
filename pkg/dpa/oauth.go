e1cf82b2b0de6d491deeb8783e3ebddbd7e8a255
Joe Strickland <joseph.a.strickland@gmail.com>
2023-12-21 13:42:48 -0500

    initial commit for client credential support

---
+++ pkg/dpa/oauth.go
@@ -0,0 +1,48 @@
+package dpa
+
+import (
+	"context"
+	"fmt"
+
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/clientcredentials"
+)
+
+type clientCredsTokenSource struct {
+	ctx  context.Context
+	conf *clientcredentials.Config
+}
+
+// OauthCredClient returns a validated Oauth2 Authentication Token based on the following provided information:
+//
+//	clientID - Username for the Application (e.g. "identity-privilege-integration-user$@example.com")
+//	clientSecret - Password for the Application Service User
+//	clientAppID - Application ID for the Oauth2 Application
+//	clientURL - URL for the Application (e.g. "example.cyberark.cloud")
+//	scope - Scope for the application (e.g. "dpa")
+//
+// Returns an oauth2.Token or error
+func OauthCredClient(clientID, clientSecret, clientAppID, clientURL string, scope []string) (*oauth2.Token, error) {
+	// Establish oauth2/clientcredentials config with user provided data
+	var credentialConfig = clientcredentials.Config{
+		ClientID:     clientID,
+		ClientSecret: clientSecret,
+		TokenURL:     "https://" + clientURL + "/oauth2/token/" + clientAppID,
+		AuthStyle:    0,
+		Scopes:       scope,
+	}
+
+	// Create tokenSource with provided configuration info
+	ts := &clientCredsTokenSource{
+		ctx:  context.Background(),
+		conf: &credentialConfig,
+	}
+
+	// Request new token from SCIM server using Client Credentials
+	authToken, err := ts.conf.Token(context.Background())
+	if err != nil {
+		return nil, fmt.Errorf("failed to obtain SCIM Oauth2 Token %w", err)
+	}
+
+	return authToken, nil
+}

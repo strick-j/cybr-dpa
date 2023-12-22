7d9074bdc54f37b3686d08180cb535273597905b
Joe Strickland <joseph.a.strickland@gmail.com>
2023-12-22 10:27:37 -0500

    Added Platform Token auth method

--- pkg/dpa/oauth.go
+++ pkg/dpa/oauth.go
@@ -46,3 +46,34 @@ func OauthCredClient(clientID, clientSecret, clientAppID, clientURL string, scop
 
 	return authToken, nil
 }
+
+// OauthPlatformToken returns a validated Oauth2 Authentication Token based on the following provided information:
+//
+//	clientID - Username for the Application (e.g. "identity-privilege-integration-user$@example.com")
+//	clientSecret - Password for the Application Service User
+//	clientURL - URL for the Application (e.g. "example.cyberark.cloud")
+//
+// Returns an oauth2.Token or error
+func OauthPlatformToken(clientID, clientSecret, clientURL string) (*oauth2.Token, error) {
+	// Establish oauth2/clientcredentials config with user provided data
+	var credentialConfig = clientcredentials.Config{
+		ClientID:     clientID,
+		ClientSecret: clientSecret,
+		TokenURL:     "https://" + clientURL + "/oauth2/platformtoken",
+		AuthStyle:    0,
+	}
+
+	// Create tokenSource with provided configuration info
+	ts := &clientCredsTokenSource{
+		ctx:  context.Background(),
+		conf: &credentialConfig,
+	}
+
+	// Request new token from server using Client Credentials
+	authToken, err := ts.conf.Token(context.Background())
+	if err != nil {
+		return nil, fmt.Errorf("failed to obtain Oauth2 Token %w", err)
+	}
+
+	return authToken, nil
+}
